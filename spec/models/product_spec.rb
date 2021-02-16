2d9c14d3482a4d81b73de3ddbbc45dc181cad238
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-16 13:45:46 +0100

    Tests go green. DB now contains expected attributes

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'DB table' do
-    it { is_expected.to have_db_column :title}
+    it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :description}
     it { is_expected.to have_db_column :price}
   end
 
   describe 'Factory' do
     it 'should have valid Factory' do
-      expect(create(:article)).to be_valid
+      expect(create(:product)).to be_valid
     end
+  end
 end
