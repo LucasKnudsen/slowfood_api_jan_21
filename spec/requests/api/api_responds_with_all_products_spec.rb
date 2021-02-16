aa1222da6b47dac14b190c72dd6a339c41032966
FoxyTony <mirantony@icloud.com>
2021-02-16 17:58:30 +0100

    Tests responds with the right data

--- spec/requests/api/api_responds_with_all_products_spec.rb
+++ spec/requests/api/api_responds_with_all_products_spec.rb
@@ -9,6 +9,19 @@ RSpec.describe 'GET /api/products', type: :request do
     it 'respond with a 200 status' do
       expect(response).to have_http_status 200
     end
+
+    it 'responds with correct title' do
+      expect(response_json["products"][0]['title']).to eq "ProductTitle"
+    end
+
+    it 'responds with correct description' do
+      expect(response_json["products"][0]['description']).to eq "Best in town"
+    end
+
+    it 'responds with correct price' do
+      expect(response_json["products"][0]['price']).to eq 49
+    end
+
   end
 
 end
