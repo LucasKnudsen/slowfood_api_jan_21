cccd9658ba3176bf5fe318aad56afebaf6808ede
Lucas Knudsen <laksteelhouse@gmail.com>
2021-02-18 10:52:01 +0100

    Sad path testing

--- spec/requests/auth/user_can_register_spec.rb
+++ spec/requests/auth/user_can_register_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'POST /api/auth', type: :request do
-  describe 'successfull registration' do
+  describe 'successful registration' do
     before do
       post '/api/auth',
       params: {
@@ -19,6 +19,25 @@ RSpec.describe 'POST /api/auth', type: :request do
 
     it 'responds with user id' do
       expect(response_json['data']['uid']). to eq "user@email.com"
+    end
+  end
+
+  describe 'unsuccessful registration' do
+    before do
+      post '/api/auth',
+      params: {
+        email: "user@email.com",
+        password: "password",
+        password_confirmation: "assword"
+      }
+    end
+
+    it 'responds with a 422 status' do
+      expect(response).to have_http_status 422
+    end
+
+    it 'response with appropriate error message' do
+      expect(response_json['errors']['full_messages'].first).to eq "Password confirmation doesn't match Password"
     end
   end
 end
